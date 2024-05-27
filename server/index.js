2b7d2656d604153d2e6a338e2de1cf5f9877c144
mridul891 <58576921+mridul891@users.noreply.github.com>
2024-05-27 11:08:29 +0530

    cors updates

--- server/index.js
+++ server/index.js
@@ -22,12 +22,11 @@ const { register } = require("./Contorllers/Register.controller");
 app.use(express.json());
 app.use(cookieParser());
 const corsOptions = {
-    origin: 'http://localhost:5173', // Replace with your React app's URL
+    origin: '*', // Replace with your React app's URL
     credentials: true, // Allow credentials (cookies, authorization headers, etc.)
 };
 app.use(cors(corsOptions));
 
-app.options('*', cors(corsOptions));
 app.use('/uploads', express.static(__dirname + "/uploads"))
 
 // salt for bcyrpt
