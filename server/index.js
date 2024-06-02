dad882bd6ab5d23acdc0001d23e6ba56c26e7b51
mridul891 <58576921+mridul891@users.noreply.github.com>
2024-06-02 22:01:02 +0530

    cors update in origin

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const dotenv = require("dotenv")
 
 // Use CORS middleware
 app.use(cors({
-    origin: "*",
+    origin: "http://localhost:5173/",
     methods: "GET,POST,PUT",
     credentials: true
 }));
